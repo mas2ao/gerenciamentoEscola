2e4340ad83481f5949d8fda249068e3b3ee4849b
John <john@john-programmer.(none)>
2013-08-27 23:14:07 -0300

    Entidade professor e turma criados.

--- LangSchool/src/java/com/entity/Professor.java
+++ LangSchool/src/java/com/entity/Professor.java
@@ -4,10 +4,33 @@
  */
 package com.entity;
 
-/**
- *
- * @author yukill
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Professor")
 public class Professor {
+    @Id
+    @Column(name = "id")
+    private int id;
     
+    @Column(name = "nome")
+    private String nome;
+    
+    public Professor(){}
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
 }
--- LangSchool/src/java/com/entity/Turma.java
+++ LangSchool/src/java/com/entity/Turma.java
@@ -4,10 +4,65 @@
  */
 package com.entity;
 
-/**
- *
- * @author yukill
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Turma")
 public class Turma {
+    @Id
+    @Column (name = "id")
+    private int id;
+    
+    @Column (name = "codigo")
+    private int codigo;
+    
+    @Column (name = "turma")
+    private String turma;
+    
+    @Column (name = "descricao")
+    private String descricao;
+    
+    @Column (name = "vagas")
+    private int vagas;
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public void setCodigo(int codigo){
+        this.codigo = codigo;
+    }
+    
+    public int getCodigo(){
+        return this.codigo;
+    }
+    
+    public void setTurma(String turma){
+        this.turma = turma;
+    }
+    
+    public String getTurma(){
+        return this.turma;
+    }
+    
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+    
+    public String getDescricao(){
+        return this.descricao;
+    }
+    
+    public void setVagas(int vagas){
+        this.vagas = vagas;
+    }
+    
+    public int getVagas(){
+        return this.vagas;
+    }
     
 }
