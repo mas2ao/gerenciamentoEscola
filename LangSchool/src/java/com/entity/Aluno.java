426d65969612ae6002f2d5469b12104df11ddd7f
danoneic <Rodrigo@Rodrigo-PC>
2013-09-06 15:32:42 -0300

    tireicomments

--- LangSchool/src/java/com/entity/Aluno.java
+++ LangSchool/src/java/com/entity/Aluno.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.entity;
 
 import javax.persistence.Column;
@@ -10,10 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author rod
- */
 @Entity
 @Table(name = "ALUNO")
 public class Aluno {
