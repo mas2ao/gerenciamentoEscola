409c014e9bcb8c1cf4d2316418895ae51673094a
YuKill <sir.yukio@gmail.com>
2013-10-04 17:12:36 -0300

    MatriculaDAO e ReposicaoAulaDAO feitos

--- LangSchool/src/java/com/DAO/DAO.java
+++ LangSchool/src/java/com/DAO/DAO.java
@@ -9,8 +9,9 @@ public abstract class DAO {
     
     public abstract String incluir(Object obj);
     public abstract String excluir(Object obj);
+    public abstract String alterar(Object obj);
     
-    public List search(Class cName, CriteriaGroup... addCrit) {
+    public List consultar(Class cName, CriteriaGroup... addCrit) {
         return ep.search(cName, addCrit);
     }
 }
--- LangSchool/src/java/com/DAO/ReposicaoAulaDAO.java
+++ LangSchool/src/java/com/DAO/ReposicaoAulaDAO.java
@@ -2,11 +2,12 @@ package com.DAO;
 
 import com.entity.ReposicaoAula;
 
-public class ReposicaoAulaDAO extends DAO{
+public class ReposicaoAulaDAO extends DAO {
+
+    private ReposicaoAula repAula;
 
     @Override
     public String incluir(Object obj) {
-        ReposicaoAula repAula;
         repAula = (ReposicaoAula) obj;
         try {
             ep.save(repAula);
@@ -19,6 +20,25 @@ public class ReposicaoAulaDAO extends DAO{
 
     @Override
     public String excluir(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        repAula = (ReposicaoAula) obj;
+        try {
+            ep.delete(repAula);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "fail";
+        }
+        return "success";
+    }
+
+    @Override
+    public String alterar(Object obj) {
+        repAula = (ReposicaoAula) obj;
+        try {
+            ep.update(repAula);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return "fail";
+        }
+        return "success";
     }
 }
